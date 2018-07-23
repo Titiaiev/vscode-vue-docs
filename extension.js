cd623bf159cd7f373b9bf96f009cc83ae8e104e0
Maksim Titiaiev <gipsmas@gmail.com>
2018-07-24 00:08:31 +0300

    отрефакторил

--- extension.js
+++ extension.js
@@ -2,8 +2,9 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-const vscode = require("vscode");
+const vscode = require("vscode"); // подключаем библиотеку vscode
 
+// шаблон для веб-отображения
 const getWebviewContent = (uri) => {
   const html = `
     <!DOCTYPE html>
@@ -24,40 +25,44 @@ const getWebviewContent = (uri) => {
     </html>
     `;
   return html;
-}
+};
 
+const getURI_of = (item = '') => {
+
+  const lang = (vscode.workspace.getConfiguration().vueDocs.lang) 
+  ? vscode.workspace.getConfiguration().vueDocs.lang : (vscode.env.language.includes('ru')) 
+                                                                        ? 'ru' : '';
+                                                                        
+  let URI_of = {
+    ['Vue']: (lang !== 'en') ? `https://${lang}.vuejs.org/v2/guide/` : 'https://vuejs.org/v2/guide/',// в англ версии к домену ничего не нужно добавлять
+    ['Vuex']: `https://vuex.vuejs.org/${lang}`,
+    ['Vue Router']: `https://router.vuejs.org/${lang}`,
+    ['Vue SSR']: `https://ssr.vuejs.org/${lang}`,
+    ['Vuetify']: 'https://vuetifyjs.com/getting-started/quick-start'
+  };
+
+  // TODO: delete logs 
+  // console.log('Выбран пункт: ', item);
+  // console.log('URI_of: ', URI_of);
+  // console.log('URI_of[item]: ', URI_of[item]);
+  // console.log("язык интерфейса vs code: ", vscode.env.language);
+  // console.log('lang: ', lang);
+  // console.log('язык в настройках: ', vscode.workspace.getConfiguration().vueDocs.lang);
+  return String(URI_of[item]);
+};
+
+// активация расширения
 const activate = (context) => {
   context.subscriptions.push(vscode.commands.registerCommand('extension.openVueDocs', () => {
-    let menuItems = ['Vue', 'Vuex', 'Vue Router', 'Vue SSR', 'Vuetify'];
+    let menuItems = ['Vue', 'Vuex', 'Vue Router', 'Vue SSR', 'Vuetify']; // возможные опции
 
+    // выбор опции из выпадающего списка
     vscode.window.showQuickPick(menuItems).then((selectedMenuItem) => {
-      const getURI_of = (item) => {
-        if(!item) { return null; }
+      if(selectedMenuItem) { // если опция выбрана - работаем, (возможна ситуация когда пользователь кликнул мимо и selectedMenuItem === undefined)
 
-        const lang = (vscode.env.language.includes('ru')) ? 'ru' : '';
-
-        let URI_of = {
-          ['Vue']: (lang) ? `https://${lang}.vuejs.org/v2/guide/` : 'https://vuejs.org/v2/guide/',
-          ['Vuex']: `https://vuex.vuejs.org/${lang}`,
-          ['Vue Router']: `https://router.vuejs.org/${lang}`,
-          ['Vue SSR']: `https://ssr.vuejs.org/${lang}`,
-          ['Vuetify']: 'https://vuetifyjs.com/getting-started/quick-start'
-        };
-
-        // TODO: delete logs 
-        // console.log('Выбран пункт: ', item);
-        // console.log('URI_of: ', URI_of);
-        // console.log('URI_of[item]: ', URI_of[item]);
-        // console.log(vscode.env.language);
-        // console.log(lang);
-
-        return URI_of[item];
-      };
-
-      const selectedURI = getURI_of(selectedMenuItem);
-      
-      if(selectedURI) {
+        // подготавливаем веб-вью панель
         const panel = vscode.window.createWebviewPanel('webDocs', selectedMenuItem, vscode.ViewColumn.One, {
+
           // разрешить загруженным сайтам использовать свои скрипты (потенциально опасно)
           // https://code.visualstudio.com/docs/extensions/webview#_scripts-and-message-passing
           enableScripts: true,
@@ -66,9 +71,11 @@ const activate = (context) => {
           // https://code.visualstudio.com/docs/extensions/webview#_persistence
           retainContextWhenHidden: true
         });
-        panel.webview.html = getWebviewContent(selectedURI);
+
+        // получаем URI соответствующий выбранному пункту меню
+        const selectedURI = getURI_of(selectedMenuItem);
+        panel.webview.html = getWebviewContent(selectedURI); // показываем ранее определённый шаблон с полученым URI
       }
-      
     });
   }));
 };
